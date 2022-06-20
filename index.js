012c2f6e7eb0d77b2e3c23abbd8826fc39ceaf96
Carlos <carlos@pop-os.localdomain>
2022-06-20 12:52:11 -0300

    try correct the error

--- index.js
+++ index.js
@@ -11,15 +11,26 @@ let users = [];
 let tweets = [];
 
 server.post("/sign-up", (request, response) => {
-    users.push(request.body);
-    response.send(users);
+    users.push(request.body)
+    response.send(users)
+    console.log(users)
 })
 
 server.post("/tweets", (request, response) => {
-    const { username, tweet } = request.body;
-    const avatar = users.find((u) => u.username === username).avatar;
-    tweets.push({ username, tweet, avatar });
+    const username = request.body.username;
+    const tweet = request.body.tweet;
+    let avatar = "";
+    let isThere = users.find((u) => u.username === username);
+    if (isThere) {
+        avatar = isThere.avatar;
+    }
+    tweets.push({ username, avatar, tweet });
+    console.log(username, tweet);
     response.send("OK");
+})
+
+server.get("/tweets", (request, response) => {
+    response.send(tweets);
 })
 
 server.listen(5000);
